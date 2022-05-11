33646de43fb0e796d047075c53984aa8337b7f3b
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-11 01:51:31 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,26 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './Styles.css'
+
+function Navbar(){
+    return(
+
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                     <a href="https://github.com/rafaelrodrigopa">
+
+                     <div className='dsmovie-contact-container'>
+                        <GithubIcon />
+                        <p className='dsmovie-contact-link'>/devsuperior</p>
+                    </div>
+
+                     </a>
+                </div>
+            </nav>
+        </header>
+
+    );
+}
+
+export default Navbar;
