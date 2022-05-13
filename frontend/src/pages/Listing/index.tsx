0c767ff105c9c64b7f5bd8e098e76a6593e77037
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-05-13 12:59:09 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,17 +1,30 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 
 function Listing(){
 
+    const [pageNumber, setPageNumber] = useState(0);
 
-    // FORMA ERRADA
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        .then(response => {
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+            console.log(data);
+        });
+    }, [])
+
+    /* FORMA ERRADA
     axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
-        });
+            const data = response.data as MoviePage;
+            setPageNumber(data.number);
+        });*/
 
     return (
         <>
